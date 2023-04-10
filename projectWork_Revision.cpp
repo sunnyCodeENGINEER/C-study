02b5d2bd835266c98b79e203fa13a3619cd3cace
sunnyCodeENGINEER <116773072+sunnyCodeENGINEER@users.noreply.github.com>
2023-04-10 15:16:28 +0000

    Creating Driver, Vehicle and DVLAOffice classes.

--- projectWork_Revision.cpp
+++ projectWork_Revision.cpp
@@ -4,6 +4,52 @@
 #include <iomanip>
 #include <stdlib.h>
 
+using namespace std;
+
+class Vehicle {
+    public:
+        string make;
+        string model;
+        string ownerLastName;
+        string ownerFirstName;
+        string registrationNumber;
+        string registrationType;
+        string vehicleType;
+        time_t registrationExpiry;
+
+        Vehicle(string make, string model, string ownerLastName, string ownerFirstName, string registrationNumber, string registrationType, string vehicleType, time_t registrationExpiry) {
+            this->make = make;
+            this->model = model;
+            this->ownerLastName = ownerLastName;
+            this->ownerFirstName = ownerFirstName;
+            this->registrationNumber = registrationNumber;
+            this->registrationType = registrationType;
+            this->vehicleType = vehicleType;
+            this->registrationExpiry = registrationExpiry;
+        }
+};
+
+class Driver {
+    public:
+        string lastName;
+        string firstName;
+        string licenseNumber;
+        time_t licenseExpiry;
+
+        Driver(string lastName, string firstName, string licenseNumber, time_t licenseExpiry) {
+            this->lastName = lastName;
+            this->firstName = firstName;
+            this->licenseNumber = licenseNumber;
+            this->licenseExpiry = licenseExpiry;
+        }
+};
+
+class DVLAOffice {
+    public:
+        vector<Vehicle> vehicles;
+        vector<Driver> drivers;
+};
+
 int main() {
 
     return 0;
